3e915611f93bde7d26075371a553b10d85e24432
Fulbabu <fulbabucse@gmail.com>
2022-11-20 14:03:34 +0600

    check payment method success

--- src/Dashboard/Payments/CheckoutForm.jsx
+++ src/Dashboard/Payments/CheckoutForm.jsx
@@ -1,13 +1,24 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 const CheckoutForm = ({ bookingData }) => {
   const [cardError, setCardError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
   const stripe = useStripe();
   const elements = useElements();
 
-  const { price } = bookingData;
+  const { price, patient, email } = bookingData;
+
+  useEffect(() => {
+    fetch("http://localhost:5000/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ price }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, [price]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -32,7 +43,25 @@ const CheckoutForm = ({ bookingData }) => {
     } else {
       setCardError("");
     }
+
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: patient,
+            email,
+          },
+        },
+      });
+
+    if (confirmError) {
+      setCardError(confirmError.message);
+      return;
+    }
+    console.log(paymentIntent);
   };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -55,7 +84,7 @@ const CheckoutForm = ({ bookingData }) => {
         <button
           className="inline-block px-4 py-1.5 bg-gradient-to-r from-primaryColor to-secondaryColor text-white font-medium text-lg leading-tight rounded-md shadow-md  hover:shadow-2xl focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition-colors duration-200 ease-in-out mt-3"
           type="submit"
-          disabled={!stripe}
+          disabled={!stripe || !clientSecret}
         >
           Pay
         </button>
